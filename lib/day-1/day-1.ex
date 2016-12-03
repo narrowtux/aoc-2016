d2ecb58606f610222d504c49d3615decbbdf46b9
Moritz Schmale <narrow.m@gmail.com>
2016-12-03 12:14:55 +0100

    hacked day 1 part 2

--- lib/day-1/day-1.ex
+++ lib/day-1/day-1.ex
@@ -2,8 +2,13 @@ defmodule Day1 do
   @day 1
   use Day
 
+  @ets_name :visited_locations
+  @ets :ets.new(@ets_name, [:set])
+
   def part_1 do
     {:ok, input} = get_input()
+    
+    # :ets.new(@ets_name, [:set])
 
     part_1(input)
   end
@@ -18,23 +23,44 @@ defmodule Day1 do
     {:ok, distance}
   end
 
-  def part_2(context) do
-    
-  end
-
   defp get_destination([], {x, y, _}), do: {x, y}
 
   defp get_destination([<< turn::binary-size(1), length::binary >> | rest], {x, y, direction}) do
     length = String.to_integer(length)
 
+
     new_direction = get_direction(direction, turn)
 
-    IO.puts [Atom.to_string(direction), " -> ", Atom.to_string(new_direction)]
+    old = {x, y}
 
     {dx, dy} = get_vector(new_direction)
     x = x + dx * length
     y = y + dy * length
+
+    walk(old, {x, y})
+    |> Enum.slice(1, 100_000)
+    |> Enum.each(&check_ets/1)
+
     get_destination(rest, {x, y, new_direction})
+  end
+
+  def walk({from_x, y}, {to_x, y}) do
+    for x <- from_x..to_x do
+      {x, y}
+    end
+  end
+
+  def walk({x, from_y}, {x, to_y}) do
+    for y <- from_y..to_y do
+      {x, y}
+    end
+  end
+
+  def check_ets({x, y}) do
+    case :ets.member(@ets, {x, y}) do
+      true -> IO.puts "Visited #{x}, #{y} for the second time"
+      false -> :ets.insert(@ets, {{x, y}, true})
+    end
   end
 
   defp manhattan_distance({x, y}), do: abs(x) + abs(y)
