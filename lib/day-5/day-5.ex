cc44ff39544ae501519a7d25aaa4c820d2ae5683
Moritz Schmale <narrow.m@gmail.com>
2016-12-05 10:00:50 +0100

    day 5

---
+++ lib/day-5/day-5.ex
@@ -0,0 +1,66 @@
+defmodule Day5 do
+  use Day
+
+  def part_1 do
+    get_input!
+    |> part_1
+  end
+
+  def part_1(input) do
+    input
+    |> get_hashes()
+    |> Enum.take(8)
+    |> Enum.map(fn
+      << "00000"::binary, letter::size(1)-binary, _::binary >> -> letter
+      _ -> nil
+    end)
+    |> :erlang.iolist_to_binary
+  end
+  
+  def part_2(input) do
+    acc = 
+      Stream.repeatedly(fn()-> " " end)
+      |> Enum.take(8)
+
+    input
+    |> get_hashes()
+    |> Enum.reduce_while(acc, fn
+      << "00000"::binary, pos::size(1)-binary, letter::size(1)-binary, _::binary >>, acc when pos in ["0", "1", "2", "3", "4", "5", "6", "7"] ->
+        pos = String.to_integer(pos) 
+        acc = case Enum.at(acc, pos) do
+          nil -> acc
+          " " -> List.replace_at(acc, pos, letter)
+          _ -> acc
+        end
+        if Enum.any?(acc, &(&1 == " ")) do
+          {:cont, acc}
+        else
+          {:halt, acc}
+        end
+      _, acc -> {:cont, acc}
+    end)
+    |> :erlang.iolist_to_binary
+  end
+
+  def get_hashes(input) do
+    Stream.iterate(0, &(&1 + 1))
+    |> Stream.map(&("#{input}#{&1}"))
+    |> Stream.map(&:crypto.hash(:md5, &1))
+    |> Stream.map(&Base.encode16(&1, case: :lower))
+    |> Stream.filter(fn 
+      << "00000"::binary, rest::binary >> -> 
+        IO.puts ["Found ", rest]
+        true
+      _ -> false
+    end)
+  end
+
+  def md5(data) do
+    :erlang.md5(data)
+    |> :erlang.bitstring_to_list
+    |> Enum.map(&(:io_lib.format("~2.16.0b", [&1])))
+    |> List.flatten
+    |> :erlang.list_to_bitstring
+  end
+
+end
