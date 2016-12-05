4fd8834d15b585af2f5e32e313144ebe4e1ccdc2
Moritz Schmale <narrow.m@gmail.com>
2016-12-05 10:03:04 +0100

    remove weird md5 copypasta

--- lib/day-5/day-5.ex
+++ lib/day-5/day-5.ex
@@ -54,13 +54,4 @@ defmodule Day5 do
       _ -> false
     end)
   end
-
-  def md5(data) do
-    :erlang.md5(data)
-    |> :erlang.bitstring_to_list
-    |> Enum.map(&(:io_lib.format("~2.16.0b", [&1])))
-    |> List.flatten
-    |> :erlang.list_to_bitstring
-  end
-
 end
